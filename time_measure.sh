4282368692272677054bd8e31b2af989b8e1b8ba
Alexey <ipodbot@rambler.ru>
2013-07-13 18:06:42 +0400

    Added zsh script to run performance tests.

---
+++ time_measure.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh 
+
+LIMIT=50000
+SENDERS=5;
+TIMET=0.0;
+TIMEN=0.0;
+RECIEVERS=100;
+
+while [ $RECIEVERS -le $LIMIT ]; do
+  (&>TIMETf time ./bin/IMRC -S $SENDERS -R $RECIEVERS >> /dev/null);
+  (&>TIMENf time ./bin/IMRC -S $SENDERS -R $RECIEVERS -T -1 >> /dev/null);
+  TIMET=$(awk '{print $0}' TIMETf);
+  TIMEN=$(awk '{print $0}' TIMENf);
+  echo "$RECIEVERS	$TIMET	$TIMEN" >> plot_sconst;
+  let RECIEVERS=RECIEVERS+100;
+  echo "Run -S $SENDERS -R $RECIEVERS.";
+done
+
+echo "Done with constant amount of recievers, starting both dynamic."
+
+RECIEVERS=100;
+SENDERS=100;
+
+while [ $RECIEVERS -le $LIMIT ]; do
+  (&>TIMETf time ./bin/IMRC -S $SENDERS -R $RECIEVERS >> /dev/null);
+  (&>TIMENf time ./bin/IMRC -S $SENDERS -R $RECIEVERS -T -1 >> /dev/null);
+  TIMET=$(awk '{print $0}' TIMETf);
+  TIMEN=$(awk '{print $0}' TIMENf);
+  echo "$RECIEVERS	$TIMET	$TIMEN" >> plot_sdyn;
+  let RECIEVERS=RECIEVERS+100;
+  let SENDERS=SENDERS+100;
+  echo "Run -S $SENDERS -R $RECIEVERS.";
+done
+
+echo "TESTS FINISHED, PLOTTING."
+
+gnuplot plots_all.gp
